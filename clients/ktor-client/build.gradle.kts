61b2a5a33f20e4892ae0eab5d2fdb92af6b79abb
Thanh Le <lenguyenthanh@hotmail.com>
2021-12-17 09:31:21 +0100

    Publish clients to jitpack

--- clients/ktor-client/build.gradle.kts
+++ clients/ktor-client/build.gradle.kts
@@ -1,5 +1,9 @@
 //apply(from = "https://raw.githubusercontent.com/arrow-kt/arrow/main/arrow-libs/gradle/publication.gradle")
 
+plugins {
+  `maven-publish`
+}
+
 dependencies {
   compileOnly(project(Libs.core))
   implementation(Libs.ktorClientCore)
@@ -7,4 +11,16 @@ dependencies {
   testImplementation(project(Libs.core))
   testImplementation(Libs.ktorClientCio)
   testImplementation(project(Libs.test))
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("maven") {
+      groupId = "com.fortysevendegrees.arrow-endpoint"
+      artifactId = "client-ktor"
+      version = "0.0.1"
+
+      from(components["java"])
+    }
+  }
 }
--- clients/spring-web-client/build.gradle.kts
+++ clients/spring-web-client/build.gradle.kts
@@ -1,9 +1,25 @@
 //apply(from = "https://raw.githubusercontent.com/arrow-kt/arrow/main/arrow-libs/gradle/publication.gradle")
 
+plugins {
+  `maven-publish`
+}
+
 dependencies {
   compileOnly(project(Libs.core))
   implementation(Libs.springBootStarterWeb)
   implementation(Libs.kotlinxCoroutines)
   testImplementation(project(Libs.core))
   testImplementation(project(Libs.test))
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("maven") {
+      groupId = "com.fortysevendegrees.arrow-endpoint"
+      artifactId = "client-spring-web"
+      version = "0.0.1"
+
+      from(components["java"])
+    }
+  }
 }
