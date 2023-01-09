b702ede44ddfd719ea294613109027001439f499
Thanh Le <lenguyenthanh@hotmail.com>
2023-01-09 20:50:26 +0100

    Yes, I did

--- clients/ktor-client/build.gradle.kts
+++ clients/ktor-client/build.gradle.kts
@@ -8,8 +8,8 @@ dependencies {
   compileOnly(project(Libs.core))
   implementation(Libs.ktorClientCore)
 
+  testImplementation(project(Libs.core))
   testImplementation(Libs.ktorClientCio)
-  testImplementation(project(Libs.test))
 }
 
 publishing {
