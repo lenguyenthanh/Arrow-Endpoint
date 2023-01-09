a3b304c43740f71ea17dce90f472a34ad511042c
Thanh Le <lenguyenthanh@hotmail.com>
2023-01-09 19:59:37 +0100

    Remove unused modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,10 @@ rootProject.name = "arrow-endpoint"
 include("core", "examples", "schema-reflect", "test")
 
 // clients
-include("clients", ":clients:ktor-client", ":clients:http4k-client", ":clients:spring-web-client", ":clients:spring-web-flux-client")
+include("clients", ":clients:ktor-client")
 
 // servers
-include("servers", ":servers:ktor-server", ":servers:spring-web-server")
+include("servers", ":servers:spring-web-server")
 
 // docs
 include("docs", ":docs:openapi-docs")
